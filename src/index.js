0df1e15988588be1189201303ef5971b36194ae8
KonstantinMinsk <Sokolov.k007@yandex.ru>
2020-03-05 20:00:26 +0300

    feat: add clean commit in solinion

--- src/index.js
+++ src/index.js
@@ -7,12 +7,4 @@ module.exports = function reverse (n) {
     let arr = String(n).split("").reverse().join("");
 
     return Number(arr)
- /*
-    if(res%2==0) {
-        res = res*(-1);
-        return res;
-    } else {
-        return res;
-    }
-        */
 }
