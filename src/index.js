e33930df354a7f52e5f7691e6ef5ebda2e47c20c
KonstantinMinsk <Sokolov.k007@yandex.ru>
2020-03-05 19:59:25 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
+    
+    if (n<0) {
+        n = -1*n
+    }
+
+    let arr = String(n).split("").reverse().join("");
+
+    return Number(arr)
+ /*
+    if(res%2==0) {
+        res = res*(-1);
+        return res;
+    } else {
+        return res;
+    }
+        */
 }
